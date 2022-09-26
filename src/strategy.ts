5e1a1273fc45680ec03857bea2c832d726f413e6
Christoph Bühler <christoph@smartive.ch>
2022-09-26 10:57:58 +0200

    test: add jest tests for strategy

--- src/strategy.ts
+++ src/strategy.ts
@@ -2,10 +2,10 @@ import axios from 'axios';
 import { Request } from 'express';
 import { ParamsDictionary } from 'express-serve-static-core';
 import { importPKCS8, SignJWT } from 'jose';
+import NodeRSA from 'node-rsa';
 import { IntrospectionResponse, Issuer } from 'openid-client';
 import { Strategy } from 'passport';
 import { ParsedQs } from 'qs';
-import NodeRSA = require('node-rsa');
 
 type ZitadelJwtProfile = {
   type: 'application';
@@ -36,8 +36,6 @@ export class ZitadelIntrospectionStrategy extends Strategy {
   name = 'zitadel-introspection';
 
   private introspect?: (token: string) => Promise<IntrospectionResponse>;
-  private jwt = '';
-  private lastCreated = 0;
 
   constructor(private readonly options: ZitadelIntrospectionOptions) {
     super();
@@ -74,44 +72,47 @@ export class ZitadelIntrospectionStrategy extends Strategy {
     }
   }
 
-  private async createPayload(token: string): Promise<Record<string, string>> {
-    if (this.options.authorization.type === 'basic') {
-      return { token };
-    }
-
-    // check if the last created time is older than 60 minutes, if so, create a new jwt.
-    if (this.lastCreated < Date.now() - 60 * 60 * 1000) {
-      const rsa = new NodeRSA(this.options.authorization.profile.key);
-      const key = await importPKCS8(rsa.exportKey('pkcs8-private-pem'), 'RSA256');
-
-      this.jwt = await new SignJWT({
-        iss: this.clientId,
-        sub: this.clientId,
-        aud: this.options.authority,
-      })
-        .setIssuedAt()
-        .setExpirationTime('1h')
-        .setProtectedHeader({
-          alg: 'RS256',
-          kid: this.options.authorization.profile.keyId,
-        })
-        .sign(key);
-      this.lastCreated = Date.now();
-    }
-
-    return {
-      client_assertion_type: 'urn:ietf:params:oauth:client-assertion-type:jwt-bearer',
-      client_assertion: this.jwt,
-      token,
-    };
-  }
-
   private async getIntrospecter() {
     const issuer = await Issuer.discover(this.options.discoveryEndpoint ?? this.options.authority);
     const introspectionEndpoint = issuer.metadata['introspection_endpoint'] as string;
 
+    let jwt = '';
+    let lastCreated = 0;
+
+    const getPayload = async (token: string): Promise<Record<string, string>> => {
+      if (this.options.authorization.type === 'basic') {
+        return { token };
+      }
+
+      // check if the last created time is older than 60 minutes, if so, create a new jwt.
+      if (lastCreated < Date.now() - 60 * 60 * 1000) {
+        const rsa = new NodeRSA(this.options.authorization.profile.key);
+        const key = await importPKCS8(rsa.exportKey('pkcs8-private-pem'), 'RSA256');
+
+        jwt = await new SignJWT({
+          iss: this.clientId,
+          sub: this.clientId,
+          aud: this.options.authority,
+        })
+          .setIssuedAt()
+          .setExpirationTime('1h')
+          .setProtectedHeader({
+            alg: 'RS256',
+            kid: this.options.authorization.profile.keyId,
+          })
+          .sign(key);
+        lastCreated = Date.now();
+      }
+
+      return {
+        client_assertion_type: 'urn:ietf:params:oauth:client-assertion-type:jwt-bearer',
+        client_assertion: jwt,
+        token,
+      };
+    };
+
     return async (token: string) => {
-      const payload = await this.createPayload(token);
+      const payload = await getPayload(token);
 
       const response = await axios.post(introspectionEndpoint, new URLSearchParams(payload), {
         auth:
---
+++ test/strategy.test.ts
@@ -0,0 +1,111 @@
+import { IntrospectionResponse } from 'openid-client';
+import passport from 'passport';
+import { ZitadelIntrospectionOptions, ZitadelIntrospectionStrategy } from '../src';
+
+const basic: ZitadelIntrospectionOptions = {
+  authority: 'https://zitadel-libraries-l8boqa.zitadel.cloud',
+  authorization: {
+    type: 'basic',
+    clientId: '180663584030785793@passport_js_strategy',
+    clientSecret: 'uQgWLVhrBq36DU36b44cs4E7oQR04nNngAPugtGstxkznURdDlb4i7Afa7vylkVi',
+  },
+};
+const jwt: ZitadelIntrospectionOptions = {
+  authority: 'https://zitadel-libraries-l8boqa.zitadel.cloud',
+  authorization: {
+    type: 'jwt-profile',
+    profile: {
+      type: 'application',
+      keyId: '180666194112545025',
+      key: '-----BEGIN RSA PRIVATE KEY-----\nMIIEpAIBAAKCAQEAsssDRiLlzyIxPE2UnKWZh52qlkQTyuEnFMraXjB791nkqyUh\njoIVMsIChJ0GYKP2J0dnScge0U3JbeD95v46j4PCkTN0i9E1HZ+qkBPJ7LffhN+1\nE9NCj+M+rjCCRzL738K6jpHBoWokEoNtTPDIvEpfXOkRJCkY/WZ77yugTF1EAPak\nKVO16cK4FDFMPNgSGbWqUaIqV/LsAdOpn4hSwZFe6qVmyEPjh1Lh+iZOL0WVyeZb\nXPChe6534XMtHTotjWaZOnpfN1tih0oOPqf/8AVSbIzqBp2ndAX9SAWWvrHP1CHa\n41UDrl2WqpeHoWEF+G/G2Lxxhhwklt2r314KPwIDAQABAoIBAAikh6P8S9+XZ4ni\ntixcVO+ZT4W0BMDp4Vm/9I5ZCaULU2JyiQy6fQvXFQgUDxrc0ilT6kpGwjab7ADD\nv4JLB8moNN+P/TJFCqxD65rLWhd3S/bAWaB7tdv+wCjrb2DBtis63onFwfZrixLa\nB46QlSOE9Nco7QsycWLOcW8+TKFkTAwfdPzpSA4f4/+rgDJvFijJu+z4kFrs40w3\nuTo5Gcxjt9KyS1a2kJoB2tuntIEAkZCIdHFhqr/Pjq2DmdG7QzfKlWFEv4Ys+jF7\nFRNR6oRVYumovh1Ja3TrBgYlZgP5Vc0xmue1mOJrcUgrnYcnPmItTIkzvDib8pgS\nv5QxMwECgYEAySWPHtTTlAMdej4erFCY2MQPI4dWaRYjKYKQ7zkWrj5s4KINl9yJ\njIdq5GkrHNbYNWbln7c7YMPrKYCDp/EN5Ymm1tpEPwLzQxLSGHjPSeaXlb9r2o6D\nVsZhF72YApmMvhoG0GRAoij1zT7kdz2wGG6gLUuFFseLZEfJZ0gogBMCgYEA44zm\nI1xl31zz8EvFD0gaU9mJs4A8dLE57G2P3Sbm+WbmF5+x011aEm2bUIPYRVQjeN0F\ngKutztIU4gJeHpmpS6WK+k6VvQ8xyj8IPQNXl8tgON/nixpnLzJKzOFsmI83rFnU\nkYBU+rtb2e/UOD8agCvGpz+eWb8ywUbjz6qKSqUCgYAmf0DAFt2To3D912vJcPd/\n7S40j49zN2BtbmbM0jFMEfiGmZ1eZkARHE3R/2rX7yqcNeBWzBvaLkUQwV7xQop1\nVv4OeuG66ZajTLPXKTALJc33RBahUstCTV+ByrCQNtEgBR0uvzE7l/lLWfEh/TV5\nx5pycNS9Al3kSHT8hmvx7wKBgQCfMR58FJMsjaIXNVr6ku3gRWtBifBBjw8/6XJh\nPGsQhj1ov48vmLp/8/BZhrOR5Qgf8Th9SR4CeBSl/RQNgmDfDERLUxkMuAmUPT6t\nOJ5aEq0RfQtG1MTlTuDnrrlDjcZcLEg7NrW858Cdmlw0sWj/zCBhN2+3x7xhXQ/Z\nY6z2BQKBgQCi6SIrOv4iB8fnpL6l6zOyYDO6cyJSp2Cln8VogICQAPy+Ps1yq3vG\njSsuBZ72StPBX97cXO5plTXxw740bwafLGIx+QUlSlZ5yfu4Dyzoo200D3w+gSan\nYFf+g/eXsH7ZEok9S0kUojvzu5v6uC1YvkKe6h24B7CjxIQRwbPMXw==\n-----END RSA PRIVATE KEY-----\n',
+      appId: '180666181378638081',
+      clientId: '180666181378703617@passport_js_strategy',
+    },
+  },
+};
+const pat = 'HLna9R_d5VN8l3jufF3m51OeJlJ7lBPZLUOx68rMW8yzELcSu5FHdQsZVOlOtsWNc-oi778';
+
+test('create strategy', () => {
+  const strategy = new ZitadelIntrospectionStrategy(basic);
+
+  expect(strategy).toBeDefined();
+  expect(strategy).toBeInstanceOf(ZitadelIntrospectionStrategy);
+  expect(strategy.name).toBe('zitadel-introspection');
+});
+
+test('fail without header object', (done) => {
+  const strategy = new ZitadelIntrospectionStrategy(basic);
+  const authenticate = passport.authenticate(strategy, (err, user, info) => {
+    try {
+      expect(user).toBeFalsy();
+      expect(info?.message).toContain('No bearer token');
+      done();
+    } catch (e) {
+      done(e);
+    }
+  });
+
+  authenticate({}, {}, () => {});
+});
+
+test('fail without authorization header', (done) => {
+  const strategy = new ZitadelIntrospectionStrategy(basic);
+  const authenticate = passport.authenticate(strategy, (err, user, info) => {
+    try {
+      expect(user).toBeFalsy();
+      expect(info?.message).toContain('No bearer token');
+      done();
+    } catch (e) {
+      done(e);
+    }
+  });
+
+  authenticate({ headers: {} }, {}, () => {});
+});
+
+test('fail without bearer header', (done) => {
+  const strategy = new ZitadelIntrospectionStrategy(basic);
+  const authenticate = passport.authenticate(strategy, (err, user, info) => {
+    try {
+      expect(user).toBeFalsy();
+      expect(info?.message).toContain('No bearer token');
+      done();
+    } catch (e) {
+      done(e);
+    }
+  });
+
+  authenticate({ headers: { authorization: 'basic foo' } }, {}, () => {});
+});
+
+test('authenticate the user against basic auth API project', (done) => {
+  const strategy = new ZitadelIntrospectionStrategy(basic);
+  const authenticate = passport.authenticate(strategy, (err, user: IntrospectionResponse, info) => {
+    try {
+      expect(err).toBeNull();
+      expect(user.active).toBe(true);
+      expect(user.sub).toBe('180665971512443137');
+      done();
+    } catch (e) {
+      done(e);
+    }
+  });
+
+  authenticate({ headers: { authorization: `bearer ${pat}` } }, {}, () => {});
+});
+
+test('authenticate the user against JWT Profile API project', (done) => {
+  const strategy = new ZitadelIntrospectionStrategy(jwt);
+  const authenticate = passport.authenticate(strategy, (err, user: IntrospectionResponse, info) => {
+    try {
+      expect(err).toBeFalsy();
+      expect(user.active).toBe(true);
+      expect(user.sub).toBe('180665971512443137');
+      done();
+    } catch (e) {
+      done(e);
+    }
+  });
+
+  authenticate({ headers: { authorization: `bearer ${pat}` } }, {}, () => {});
+});
